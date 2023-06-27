29b52c996f9fdb5af22ba038436862096e9f609a
Dylan Alfonso <dylanalfonso0213@gmail.com>
2023-06-27 20:37:25 +0000

    Added a prompt function

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import requests
 import os
 import json
 
-KEY = os.environ.get('TOMORROW_KEY')
-LOCATION = '60448 US'
-TIMESTEPS = '1d'
-UNITS = 'imperial'
+# KEY = os.environ.get('TOMORROW_KEY')
+# KEY = 'ao57EKrfioCgEyxOdx4g8g3nDvm1egGQ'
+# LOCATION = '60448 US'
+# TIMESTEPS = '1d'
+# UNITS = 'imperial'
 
-url = f"https://api.tomorrow.io/v4/weather/forecast?location={LOCATION}&timesteps={TIMESTEPS}&units={UNITS}&apikey={KEY}"
+# url = f"https://api.tomorrow.io/v4/weather/forecast?location={LOCATION}&timesteps={TIMESTEPS}&units={UNITS}&apikey={KEY}"
 
-headers = {"accept": "application/json"}
+# headers = {"accept": "application/json"}
 
-response = requests.get(url, headers=headers)
-response_data = json.loads(response.text)
+# response = requests.get(url, headers=headers)
+# response_data = json.loads(response.text)
 
-formatted_response = json.dumps(response_data, indent=4)
-print(formatted_response)
+# formatted_response = json.dumps(response_data, indent=4)
+# print(formatted_response)
+
+def prompt():
+    location = input('Would you like to input a zip code or a city name? (Z/C) ')
+    if location == 'Z':
+        return readZip()
+    elif location == 'C':
+        return readCity()
+    else:
+        print("Sorry, that didn't work, make sure you're inputting an appropriate string")
+        return prompt()
 
 # temperatureMax
 # temperatureMin
