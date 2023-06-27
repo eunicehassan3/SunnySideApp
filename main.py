66588d6c8e54c602a332b3180d8b99dcf275ac29
dsantiago8 <93739360+dsantiago8@users.noreply.github.com>
2023-06-27 15:47:56 -0700

    readZip method

--- main.py
+++ main.py
@@ -17,6 +17,23 @@ response_data = json.loads(response.text)
 formatted_response = json.dumps(response_data, indent=4)
 print(formatted_response)
 
+def readZip():
+  while True:
+        zip_code = input('Enter the zip code: ')
+        try:
+            zip_code = int(zip_code)
+            if zip_code > 0:
+                location = str(zip_code) + ' US'
+                return location
+                break
+            else:
+                print("Invalid zip code")
+        except ValueError:
+              print("Does nothing")
+        
+    
+  
+
 # temperatureMax
 # temperatureMin
 # UVIndex
