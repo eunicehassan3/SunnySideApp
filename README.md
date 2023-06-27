d1a0512cec26fc9fe5a80c09be5058a16f698436
eunicehassan3 <euniceh.info@gmail.com>
2023-06-27 19:48:58 +0000

    Main python file added, API example added

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # SunnySideApp : Serving You Weather With A Smile
 
 SunnySideApp is the number one weather App for accessible weather. 
+The program's format is
 A simple run of commands can give you plenty of different weather services from real time weather to
 daily forecast for the next five days!
---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import os
+import json
+
+KEY = os.environ.get('TOMORROW_KEY')
+LOCATION = '60448 US'
+TIMESTEPS = '1d'
+UNITS = 'imperial'
+
+url = f"https://api.tomorrow.io/v4/weather/forecast?location={LOCATION}&timesteps={TIMESTEPS}&units={UNITS}&apikey={KEY}"
+
+headers = {"accept": "application/json"}
+
+response = requests.get(url, headers=headers)
+response_data = json.loads(response.text)
+
+formatted_response = json.dumps(response_data, indent=4)
+print(formatted_response)
+
+# temperatureMax
+# temperatureMin
+# UVIndex
+# Visibility
+# rainAccumulationAvg
+# windSpeedMax
+# WindSpeedMin
+# WindSpeedAvg
