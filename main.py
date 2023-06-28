f71bcd15e7daa64aa14abd0657ad2f8b947eecc8
dsantiago8 <93739360+dsantiago8@users.noreply.github.com>
2023-06-28 10:05:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import requests
 import os
 import json
 
-KEY = os.environ.get('TOMORROW_KEY')
-LOCATION = '60448 US'
-TIMESTEPS = '1d'
-UNITS = 'imperial'
+#KEY = os.environ.get('TOMORROW_KEY')
+#LOCATION = '60448 US'
+#TIMESTEPS = '1d'
+#UNITS = 'imperial'
 
-url = f"https://api.tomorrow.io/v4/weather/forecast?location={LOCATION}&timesteps={TIMESTEPS}&units={UNITS}&apikey={KEY}"
+#url = f"https://api.tomorrow.io/v4/weather/forecast?location={LOCATION}&timesteps={TIMESTEPS}&units={UNITS}&apikey={KEY}"
 
-headers = {"accept": "application/json"}
+#headers = {"accept": "application/json"}
 
-response = requests.get(url, headers=headers)
-response_data = json.loads(response.text)
+#response = requests.get(url, headers=headers)
+#response_data = json.loads(response.text)
 
-formatted_response = json.dumps(response_data, indent=4)
-print(formatted_response)
+#formatted_response = json.dumps(response_data, indent=4)
+#print(formatted_response)
 
 def readZip():
-  while True:
+    while True:
         zip_code = input('Enter the zip code: ')
         try:
             zip_code = int(zip_code)
