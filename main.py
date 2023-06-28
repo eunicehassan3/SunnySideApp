7a5da065974df26b46d05cbd7aeed65c6c74411c
eunicehassan3 <112098151+eunicehassan3@users.noreply.github.com>
2023-06-28 11:12:21 -0500

    fixed problems in the readCity() function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+import urllib.parse
 
 KEY = os.environ.get('TOMORROW_KEY')
 LOCATION = '60448 US'
@@ -18,11 +19,10 @@ formatted_response = json.dumps(response_data, indent=4)
 print(formatted_response)
 
 def readCity():
-  while True:
-    city = input("Please enter the name of your city: ")
-    location = city
-    return location
-    break
+  city = input("Please enter the name of your city: ")
+  location = urllib.parse.quote(city)
+  return location
+  break
 
 # temperatureMax
 # temperatureMin
