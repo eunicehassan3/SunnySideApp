6fc9822558ffbd1ebe0a0135c3d5df87ce6b6dec
Dylan Alfonso <dylanalfonso0213@gmail.com>
2023-06-28 16:54:47 +0000

    Fixed things being passed

--- main.py
+++ main.py
@@ -23,24 +23,26 @@ def prompt():
     location = input(
         'Would you like to input a zip code or a city name? (Z/C) ')
     if location == 'Z':
-        return readZip(location)
+        return readZip()
     elif location == 'C':
-        return readCity(location)
+        return readCity()
     else:
         print("Sorry, that didn't work, "
               "make sure you're inputting an appropriate string")
         return prompt()
 
 
-def readCity(location):
+def readCity():
     # Need to implement, takes in 'C', returns formatted location,
     # having a return value to pass the styling
+    location = ""
     return location
 
 
-def readZip(location):
+def readZip():
     # Need to implement, takes in 'Z', returns formatted zip code
     # having a return value to pass the styling
+    location = ""
     return location
 
 
