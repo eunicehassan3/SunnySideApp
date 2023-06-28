7af13025a1ea250a63929040a4fd97e9bc1e7e99
eunicehassan3 <112098151+eunicehassan3@users.noreply.github.com>
2023-06-28 11:18:50 -0500

    fixed the styling errors

--- main.py
+++ main.py
@@ -3,26 +3,27 @@ import os
 import json
 import urllib.parse
 
-KEY = os.environ.get('TOMORROW_KEY')
-LOCATION = '60448 US'
-TIMESTEPS = '1d'
-UNITS = 'imperial'
+# KEY = os.environ.get('TOMORROW_KEY')
+# LOCATION = '60448 US'
+# TIMESTEPS = '1d'
+# UNITS = 'imperial'
 
-url = f"https://api.tomorrow.io/v4/weather/forecast?location={LOCATION}&timesteps={TIMESTEPS}&units={UNITS}&apikey={KEY}"
+# url = f"https://api.tomorrow.io/v4/weather/forecast?location={LOCATION}&timesteps={TIMESTEPS}&units={UNITS}&apikey={KEY}"
 
-headers = {"accept": "application/json"}
+# headers = {"accept": "application/json"}
 
-response = requests.get(url, headers=headers)
-response_data = json.loads(response.text)
+# response = requests.get(url, headers=headers)
+# response_data = json.loads(response.text)
 
-formatted_response = json.dumps(response_data, indent=4)
-print(formatted_response)
+# formatted_response = json.dumps(response_data, indent=4)
+# print(formatted_response)
+
 
 def readCity():
-  city = input("Please enter the name of your city: ")
-  location = urllib.parse.quote(city)
-  return location
-  break
+      city = input("Please enter the name of your city: ")
+      location = urllib.parse.quote(city)
+      return location
+      break
 
 # temperatureMax
 # temperatureMin
